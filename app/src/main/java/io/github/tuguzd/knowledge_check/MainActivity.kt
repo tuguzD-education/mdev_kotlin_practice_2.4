3580e2c668130d51ba2107ba1ca8ae9244f1f4df
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-02-26 10:56:12 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tuguzd/knowledge_check/MainActivity.kt
@@ -0,0 +1,65 @@
+package io.github.tuguzd.knowledge_check
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val kmEditText = findViewById<EditText>(R.id.km)
+        val mEditText = findViewById<EditText>(R.id.m)
+        val dmEditText = findViewById<EditText>(R.id.dm)
+        val smEditText = findViewById<EditText>(R.id.sm)
+        val mmEditText = findViewById<EditText>(R.id.mm)
+
+        val inspectButton = findViewById<Button>(R.id.inspect)
+        val resultTextView = findViewById<TextView>(R.id.result)
+        val answerImageView = findViewById<ImageView>(R.id.answer)
+
+        inspectButton.setOnClickListener {
+            fun good() {
+                answerImageView.setImageResource(R.drawable.cool)
+                resultTextView.apply {
+                    text = getString(R.string.good)
+                    setTextColor(Color.BLUE)
+                }
+            }
+
+            fun bad() {
+                answerImageView.setImageResource(R.drawable.bad)
+                resultTextView.apply {
+                    text = getString(R.string.bad)
+                    setTextColor(Color.RED)
+                }
+            }
+
+            try {
+                val km = kmEditText.text.toString().toDouble()
+                val m = mEditText.text.toString().toDouble()
+                val dm = dmEditText.text.toString().toDouble()
+                val sm = smEditText.text.toString().toDouble()
+                val mm = mmEditText.text.toString().toDouble()
+
+                if (km * 1000 == m && m * 10 == dm && dm * 10 == sm && sm * 10 == mm) good()
+                else bad()
+            } catch (_: NumberFormatException) {
+                bad()
+            }
+        }
+
+        answerImageView.setOnClickListener {
+            if (answerImageView.alpha < 0.1f) {
+                answerImageView.alpha = 1f
+                return@setOnClickListener
+            }
+            answerImageView.alpha -= 0.1f
+        }
+    }
+}
